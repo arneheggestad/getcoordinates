5382717adae43f5c1ffbe7f1d61a5a84da864fa4
Arne Heggestad <arne.heggestad@gmail.com>
2014-12-15 12:43:22 -0500

    Added geocoder call in index

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ var csv = require('csv-to-json');
 // local file/module dependencies
 var config = require('./config.js'),
 		keys = require('./keys.js'),
-		distance = require('./lib/distance.js')
+		distance = require('./lib/distance.js'),
+		geocoder = require('./lib/geocoder.js')
 		;
 
+// finding stops near the studio
 var stops = csv.parse('./stops.txt');
-
 var start = [35.034374, -85.302557]; // starting point for search
 var radius = 300; // maximum distance in meters
-
 var closeStops = [];
 for (var i = 0; i < stops.length; i++) {
 	if (distance.haversine(start, [stops[i].stop_lat, stops[i].stop_lon]) <= radius) {
@@ -22,3 +22,14 @@ for (var j = 0; j < closeStops.length; j++) {
 	var stop = [closeStops[j].stop_lat, closeStops[j].stop_lon];
 	console.log(closeStops[j].stop_name + ': ' + distance.haversine(start, stop) + ' meters');
 }
+
+// testing CSV API call for geocoder.us
+var studio = '400 E. Main St, Chattanooga, TN';
+geocoder.csv(studio, function (err, coords) {
+	if (err) {
+		console.log('Error! ' + err);
+	}
+	if (coords) {
+		console.log('Lat: ' + coords[0] + '. Lon: ' + coords[1] + '.');
+	}
+})
