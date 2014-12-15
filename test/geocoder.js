dbb987ec42f4793287bd4f91dbf4fa11aa01ffec
Arne Heggestad <arne.heggestad@gmail.com>
2014-12-15 12:34:40 -0500

    added csv method for geocoder.us API

--- test/geocoder.js
+++ test/geocoder.js
@@ -2,3 +2,18 @@ var should = require('should'),
 		geocoder = require('../lib/geocoder.js')
 		;
 
+var testAddress = '1600 Pennsylvania Ave, Washington DC',
+		csvtestResult = '38.898748,-77.037684,1600 Pennsylvania Ave NW,Washington,DC,20502\n';
+var csvtestArray = [38.898748, -77.037684];
+
+describe('geocoder', function () {
+	describe('csv()', function () {
+		it('should return csv version of the White House', function (done) {
+			geocoder.csv(testAddress, function (err, result) {
+				result.should.eql(csvtestArray);
+				done();
+			});
+		});
+	});
+
+});
