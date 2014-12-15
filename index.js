69164c698cc755d9bc3611fea936e31c4a29905b
Arne Heggestad <arne.heggestad@gmail.com>
2014-12-15 16:08:07 -0500

    using geocoder to find stops in a radius

--- index.js
+++ index.js
@@ -10,26 +10,25 @@ var config = require('./config.js'),
 
 // finding stops near the studio
 var stops = csv.parse('./stops.txt');
-var start = [35.034374, -85.302557]; // starting point for search
-var radius = 300; // maximum distance in meters
+var radius = 100; // maximum distance in meters
 var closeStops = [];
-for (var i = 0; i < stops.length; i++) {
-	if (distance.haversine(start, [stops[i].stop_lat, stops[i].stop_lon]) <= radius) {
-		closeStops.push(stops[i]);
-	}
-}
-for (var j = 0; j < closeStops.length; j++) {
-	var stop = [closeStops[j].stop_lat, closeStops[j].stop_lon];
-	console.log(closeStops[j].stop_name + ': ' + distance.haversine(start, stop) + ' meters');
-}
 
 // testing CSV API call for geocoder.us
-var studio = '400 E. Main St, Chattanooga, TN';
-geocoder.csv(studio, function (err, coords) {
+var start = '400 e main st, chattanooga, tn';
+geocoder.csv(start, function (err, coords) {
 	if (err) {
 		console.log('Error! ' + err);
 	}
 	if (coords) {
-		console.log('Lat: ' + coords[0] + '. Lon: ' + coords[1] + '.');
+		console.log(coords);
+		for (var i = 0; i < stops.length; i++) {
+			if (distance.haversine(coords, [stops[i].stop_lat, stops[i].stop_lon]) <= radius) {
+				closeStops.push(stops[i]);
+			}
+		}
+		for (var j = 0; j < closeStops.length; j++) {
+			var stop = [closeStops[j].stop_lat, closeStops[j].stop_lon];
+			console.log(closeStops[j].stop_name + ': ' + distance.haversine(coords, stop) + ' meters');
+		}
 	}
 })
--- test/geocoder.js
+++ test/geocoder.js
@@ -3,17 +3,24 @@ var should = require('should'),
 		;
 
 var testAddress = '1600 Pennsylvania Ave, Washington DC',
-		csvtestResult = '38.898748,-77.037684,1600 Pennsylvania Ave NW,Washington,DC,20502\n';
-var csvtestArray = [38.898748, -77.037684];
+		csvtestResult = '38.898748,-77.037684,1600 Pennsylvania Ave NW,Washington,DC,20502\n',
+		csvtestCoords = [38.898748, -77.037684],
+		noResults = 'chattanooga, tn'
+		;
 
 describe('geocoder', function () {
 	describe('csv()', function () {
 		it('should return csv version of the White House', function (done) {
 			geocoder.csv(testAddress, function (err, result) {
-				result.should.eql(csvtestArray);
+				result.should.eql(csvtestCoords);
 				done();
 			});
 		});
+		it('should return no data', function (done) {
+			geocoder.csv(noResults, function (err, result) {
+				result.should.eql([]);
+			})
+		})
 	});
 
 });
