a6766721f39b13cb28a61068732cceba1838f0e8
Arne Heggestad <arne.heggestad@gmail.com>
2014-12-17 10:12:28 -0500

    created config dir

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // other module dependcies
 
 // local file/module dependencies
-var config = require('./config.js'),
+var config = require('./config/config.js'),
 		distance = require('./lib/distance.js'),
-		geocoder = require('./lib/geocoder.js')
+		geocoder = require('./lib/geocoder.js'),
+		keys = require('./config/keys.js')
 		;
 
 var getCoordinates = function (address, options, callback) {
