5608f2192c566d0c30ab3603e448b3ca18ada933
Arne Heggestad <arne.heggestad@gmail.com>
2014-12-17 09:33:16 -0500

    mapquest working for single point

--- test/findStops.js
+++ test/findStops.js
@@ -9,30 +9,30 @@ var config = require('../config.js'),
 
 var stopsNearStudio = [1086, 1191];
 
-describe('find-stops-near-studio-via-csv', function () {
-	it('should find two stops', function (done) {
-		// finding stops near the studio
-		var stops = csv.parse('test/data/stops.txt');
-		var radius = 100; // maximum distance in meters
-		var closeStops = [];
-		var address = '400 e main st, chattanooga, tn';
-		geocoder.csv(address, function (err, coords) {
-			for (var i = 0; i < stops.length; i++) {
-				var testCoords = {
-					lat: stops[i].stop_lat,
-					lon: stops[i].stop_lon
-				};
-				if (distance.haversine(coords, testCoords) <= radius) {
-					closeStops.push(stops[i]);
-				}
-			}
-			var nearbyStops = [];
-			for (var j = 0; j < closeStops.length; j++) {
-				nearbyStops.push(closeStops[j].stop_id);
-			};
-			nearbyStops.should.eql(stopsNearStudio);
-			return done();
-		});
-	});
-});
+// describe('find-stops-near-studio-via-csv', function () {
+// 	it('should find two stops', function (done) {
+// 		// finding stops near the studio
+// 		var stops = csv.parse('test/data/stops.txt');
+// 		var radius = 100; // maximum distance in meters
+// 		var closeStops = [];
+// 		var address = '400 e main st, chattanooga, tn';
+// 		geocoder.csv(address, function (err, coords) {
+// 			for (var i = 0; i < stops.length; i++) {
+// 				var testCoords = {
+// 					lat: stops[i].stop_lat,
+// 					lon: stops[i].stop_lon
+// 				};
+// 				if (distance.haversine(coords, testCoords) <= radius) {
+// 					closeStops.push(stops[i]);
+// 				}
+// 			}
+// 			var nearbyStops = [];
+// 			for (var j = 0; j < closeStops.length; j++) {
+// 				nearbyStops.push(closeStops[j].stop_id);
+// 			};
+// 			nearbyStops.should.eql(stopsNearStudio);
+// 			return done();
+// 		});
+// 	});
+// });
 
--- test/mapquest.js
+++ test/mapquest.js
@@ -5,16 +5,19 @@ var should = require('should'),
 var testAddress = '1600 Pennsylvania Ave NW, Washington DC',
 		testResult = '38.898748,-77.037684,1600 Pennsylvania Ave NW,Washington,DC,20502\n',
 		testCoords = {
-				lat: 38.898748, 
-				lon: -77.037684},
+		// note: these are *slightly* different than those from geocoder.us
+			lat: 38.897699, 
+			lon: -77.036553},
 		noResults = '123 Fake Street, Anytown, TN',
 		multipleResults = '400 Main St, Chattanooga, TN'
 		;
 
 describe('open-API', function () {
-	describe('white house', function (done) {
+	it('should return coordinates for the white house', function (done) {
 		mapquest.open(testAddress, function (err, coords) {
-			coords.should.eql(testCoods);
+			console.log(err);
+			coords.should.eql(testCoords);
+			done();
 		})
 	})
 })
